3db3ea9bb10ed71de9ad91651b81774ae76b256f
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-20 17:01:15 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'stations/index'
-  get 'stations/show'
-
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/new'
-  post 'trips/create'
-  patch 'trips/end_trip'
-
+  resources :stations, only: [:index, :show]
   resources :bikes, only: [:index, :show]
-  resources :user, only: [:index, :show]
+  resources :users, only: [:index, :show]
+  resources :trips, only: [:index, :show, :new, :create]
+  patch 'trips', to: 'trips#end_trip'
 end
