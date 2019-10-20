280e8dda8d3a5bc9b9ddc7fa26d5e7ed906787c3
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-20 12:33:34 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trips, except: [:destroy]
+  resources :stations, only: [:index, :show]
+  resources :bikes, only: [:index, :show]
+  resources :user, only: [:index, :show]
 end
