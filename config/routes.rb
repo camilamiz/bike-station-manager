ca2b149349aa158614877ecf2dbbc29d20e8ac14
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-20 13:18:54 -0300

    Add StationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  get 'stations/index'
+  get 'stations/show'
   get 'trips/index'
   get 'trips/show'
   get 'trips/new'
   post 'trips/create'
   patch 'trips/end_trip'
-  resources :trips, except: [:destroy]
-  resources :stations, only: [:index, :show]
+
   resources :bikes, only: [:index, :show]
   resources :user, only: [:index, :show]
 end
