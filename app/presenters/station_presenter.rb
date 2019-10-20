7ac7872d3c813c63dc7b3c76e2f10e424360f905
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-20 16:31:53 -0300

    Add new methd

--- app/presenters/station_presenter.rb
+++ app/presenters/station_presenter.rb
@@ -5,7 +5,17 @@ class StationPresenter < SimpleDelegator
 
     def available_bikes_in_station
         in_station_id = BikeStatus.where(description: 'In station').first.id
-        Bike.where(station_id: @station.id, bike_status_id: in_station_id).count
+        Bike.where(station_id: @station.id, bike_status_id: in_station_id)
+    end
+
+    def available_bikes_in_station_by_id
+        bike_ids = []
+        in_station_id = BikeStatus.where(description: 'In station').first.id
+        bikes = Bike.where(station_id: @station.id, bike_status_id: in_station_id)
+        bikes.each do |bike|
+            bike_ids << bike.id
+        end
+        return bike_ids
     end
 
     def spots_available
