8317494f69c1395812249e6a59eb841a043ce930
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-20 17:01:54 -0300

    Add new and create methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,9 +9,26 @@ class TripsController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
+    @users = []
+
+    User.all.each do |user|
+      @users << [user.id, user.name]
+    end
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    @trip.start_time = Time.new
+    if @trip.save
+      bike_status = BikeStatus.where(description: 'In use').first
+      bike = Bike.find(trip_params[:bike_id])
+      bike.bike_status_id = bike_status.id
+      bike.save
+      redirect_to trip_path(@trip), notice: 'Bike alugada!'
+    else
+      render :new
+    end
   end
 
   def end_trip
@@ -20,5 +37,9 @@ class TripsController < ApplicationController
   private
   def set_trip
     @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:origin_station_id, :bike_id, :user_id)
   end
 end
