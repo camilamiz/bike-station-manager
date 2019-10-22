2d2a78bcea2afa3b6f79e489a3ea6e21fe46e79a
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-21 21:39:52 -0300

    Add bikes controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'stations#index'
-  
   get '/stations', to: 'stations#index'
   get '/stations/:id', to: 'stations#show'
   
   resources :trips, except: [:destroy]
+  resources :bikes, only: [:index, :show, :edit, :update]
 
-  # resources :bikes, only: [:index, :show]
   # resources :users, only: [:index, :show]
 end
