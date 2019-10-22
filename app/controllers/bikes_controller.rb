03bdc3ff6039164a9d4112aaa5f21c200badc781
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-21 22:18:23 -0300

    Add show view and edit as partial

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,15 +1,32 @@
 class BikesController < ApplicationController
+  before_action :set_bike, only: [:show, :edit, :update]
+
   def index
     @bikes = Bike.all
   end
 
   def show
     @bike = Bike.find(params[:id])
+    @available_statuses = []
+    available_status_objects = BikeStatus.all - BikeStatus.where(id: @bike.bike_status_id)
+    
+    available_status_objects.each do |status|
+      @available_statuses << [status.id, status.description]
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:station_id, :bike_status_id)
   end
 end
