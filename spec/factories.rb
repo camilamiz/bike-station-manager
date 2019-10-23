2d3cf3f02d79768e1ae9447b7ff240272abce1d7
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-23 09:10:59 -0300

    Add factories for station, bike status and bike

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
-    factory :bike_status do
-      description "In station"
-    end
+  factory :station do
+    capacity 20
+    neighborhood {"Vila Mariana"}
+  end
+  factory :bike_status do
+    description {"In station"}
+  end
+  factory :bike do
+    bike_status
+    station
+  end
 end
