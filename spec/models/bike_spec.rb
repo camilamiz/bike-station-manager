c3ca96a1a6a6d397568070548421e7adf6b2dd6a
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-23 09:24:34 -0300

    Remove unnecessary global variable

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Bike, :type => :model do
     it "is valid with valid attributes" do
-      @bike1 = build(:bike)
-      expect(@bike1).to be_valid
+      bike1 = build(:bike)
+      expect(bike1).to be_valid
     end
 
     it "belongs to a station" do
