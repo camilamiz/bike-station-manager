68f7721f0854b8ac1f398cb39e983831314fbc06
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-22 21:08:47 -0300

    Add method to find bikes that need repair

--- app/presenters/station_presenter.rb
+++ app/presenters/station_presenter.rb
@@ -8,6 +8,11 @@ class StationPresenter < SimpleDelegator
         Bike.where(station_id: @station.id, bike_status_id: in_station_id)
     end
 
+    def need_to_be_repaired
+        needs_repair_id = BikeStatus.where(description: 'Needs repair').first.id
+        Bike.where(station_id: @station.id, bike_status_id: needs_repair_id)
+    end
+
     def available_bikes_in_station_by_id
         bike_ids = []
         in_station_id = BikeStatus.where(description: 'In station').first.id
@@ -19,6 +24,7 @@ class StationPresenter < SimpleDelegator
     end
 
     def spots_available
-        @station.capacity - self.available_bikes_in_station.count
+        needs_repair_id = BikeStatus.where(description: 'Needs repair').first.id
+        @station.capacity - self.available_bikes_in_station.count - self.need_to_be_repaired.count
     end
 end
