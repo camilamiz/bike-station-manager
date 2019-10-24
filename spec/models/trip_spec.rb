2abf546741ba2bf03999b3a8d39817a810c0acd9
camila mizumoto <camila.yuri.m@gmail.com>
2019-10-23 22:10:49 -0300

    Add trip tests

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,19 +1,48 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe Trip, type: :model do
-  context 'when saving a new trip' do
-    it 'the rented bike cannot need repair or in use' do
-      bike_status = BikeStatus.create!(description: 'Needs repair')
-      station = Station.create!
-      user = User.create!
-      bike = Bike.create!(bike_status_id: bike_status, station_id: station.id)
-      trip = Trip.new(
-        origin_station_id: station_id,
-        start_time: datetime.now,
-        user_id: user.id,
-        bike_id: bike.id
-      )
-      expect(trip.save).to_not be_valid
+RSpec.describe Trip, :type => :model do
+    it "is valid with valid attributes" do
+      trip1 = build(:trip)
+      expect(trip1).to be_valid
     end
-  end
+
+    it "has a start time" do
+        trip2 = build(:trip, start_time: nil)
+        expect(trip2).to_not be_valid
+    end
+
+    it "has a bike_id" do
+      trip2 = build(:trip, bike_id: nil)
+      expect(trip2).to_not be_valid
+    end
+
+    it "has a origin_station_id" do
+      trip2 = build(:trip, origin_station_id: nil)
+      expect(trip2).to_not be_valid
+    end
+
+    it "has a user_id" do
+      trip2 = build(:trip, user_id: nil)
+      expect(trip2).to_not be_valid
+    end
+
+    it "starts with a bike status 'In station'" do
+      bike_status = create(:bike_status)
+      bike = create(:bike, bike_status_id: bike_status.id)
+      trip2 = build(:trip, bike_id: bike.id)
+      expect(trip2).to be_valid
+    end
+
+    it "cannot start with a bike status different from 'In station'" do
+      bike_status = build(:bike_status, description: 'Needs repair')
+      bike = build(:bike, bike_status_id: bike_status.id)
+      trip2 = build(:trip, bike_id: bike.id)
+      expect(trip2).to_not be_valid
+    end
+
+    it "cannot finish where it started" do
+      station = build(:station)
+      trip2 = build(:trip, origin_station_id: station.id, destiny_station_id: station.id)
+      expect(trip2).to_not be_valid
+    end
 end
